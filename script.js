70ab1af10d0e6a386df5590de7ac9c94081af438
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2022-10-25 16:36:10 -0400

    Got basic theme changing working with JS.
    Made some super basic JS event listeners on the span containing 1, 2, & 3. The event changes the "theme-x" class on the body tag.
    
    Still a lot of problems to fix, including equals and delres colors.

---
+++ script.js
@@ -0,0 +1,17 @@
+const theme1 = document.querySelector('#theme-1')
+const theme2 = document.querySelector('#theme-2')
+const theme3 = document.querySelector('#theme-3')
+const body = document.querySelector('body')
+
+theme1.addEventListener('click', function (e) {
+    body.className = "theme-1"
+    console.log('theme 1')
+});
+theme2.addEventListener('click', function (e) {
+    body.className = "theme-2"
+    console.log('theme 2')
+});
+theme3.addEventListener('click', function (e) {
+    body.className = "theme-3"
+    console.log('theme 3')
+});
