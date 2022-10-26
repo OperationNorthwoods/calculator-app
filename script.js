f18984ff27cb8e66c744ea43c62f28234d4e8fa8
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2022-10-26 16:51:26 -0400

    Started 2 things, both in progress.
    Started on numpad event listeners. Started adding hover colors to theme 1 & 2.
    
    JS:
    Got both Del and Res to work as they are selected on their own. all other variables need to be iterated over before I can add event listeners.

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ theme3.addEventListener('click', function (e) {
     console.log('theme 3')
 });
 
+// ============================================================ //
+
+const number = document.querySelectorAll('.number')
+const operator = document.querySelectorAll('.operator')
+const delres = document.querySelectorAll('.delres')
+const del = document.querySelector('#del')
+const res = document.querySelector('#res')
+const equals = document.querySelectorAll('.equals')
+
+// number.addEventListener('click', function (e) {
+//     console.log('number')
+// });
+// operator.addEventListener('click', function (e) {
+//     console.log('operator')
+// });
+// delres.addEventListener('click', function (e) {
+//     console.log('delres')
+// });
+del.addEventListener('click', function (e) {
+    console.log('del')
+});
+res.addEventListener('click', function (e) {
+    console.log('res')
+});
+// equals.addEventListener('click', function (e) {
+//     console.log('equals')
+// });
+
+//=============//
+// for (let i=1; i>10; i++) {
+//     let n = document.querySelectorAll('.number')
+//     n. //trying to iterator over all numbers and pull out the .innerText to determine their values.
+//        //I could also just set them manually and it would be easier
+// }
